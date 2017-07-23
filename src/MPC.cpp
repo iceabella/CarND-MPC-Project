95226edc9b862862c649a43dae0826fe3bd94c29
iceabella <isabellamariaellinor@gmail.com>
2017-07-23 17:04:29 +0200

    Add operator() function in FG_eval class (MPC.cpp).

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -6,8 +6,8 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 0;
-double dt = 0;
+size_t N = 20;
+double dt = 0.1;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -33,6 +33,69 @@ class FG_eval {
     // `fg` a vector of the cost constraints, `vars` is a vector of variable values (state & actuators)
     // NOTE: You'll probably go back and forth between this function and
     // the Solver function below.
+
+
+    // DEFINE COST ********* ADD MORE ADVANCED COST **********
+    for (int t = 0; t < N; t++) {
+      fg[0] += CppAD::pow(vars[cte_start + t], 2);
+      fg[0] += CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
+    }
+    
+    
+    // SET UP MODEL CONSTRAINTS
+    
+    // Initial constraints
+    //
+    // We add 1 to each of the starting indices due to cost being located at
+    // index 0 of `fg`.
+    // This bumps up the position of all the other values.
+    fg[1 + x_start] = vars[x_start];
+    fg[1 + y_start] = vars[y_start];
+    fg[1 + psi_start] = vars[psi_start];
+    fg[1 + v_start] = vars[v_start];
+    fg[1 + cte_start] = vars[cte_start];
+    fg[1 + epsi_start] = vars[epsi_start];
+    
+    // create derivative of polynomial i.e. f'(x)
+    Eigen::VectorXd fp_coeffs(coeffs.size()-1);
+    for (int i = 0; i < coeffs.size()-1; i++) 
+      fp_coeffs(i) = coeffs(i+1)*(i+1);
+    //std::cout << "fp_coeffs: " << fp_coeffs << std::endl;
+  
+    // The rest of the constraints
+    for (int t = 1; t < N; t++) {
+      // define parameters for readability
+      AD<double> x1 = vars[x_start + t];
+      AD<double> y1 = vars[y_start + t];
+      AD<double> psi1 = vars[psi_start + t];
+      AD<double> v1 = vars[v_start + t];      
+      AD<double> cte1 = vars[cte_start + t];   
+      AD<double> epsi1 = vars[epsi_start + t];
+
+      AD<double> x0 = vars[x_start + t - 1];
+      AD<double> y0 = vars[y_start + t - 1];
+      AD<double> psi0 = vars[psi_start + t - 1];
+      AD<double> v0 = vars[v_start + t - 1];     
+      AD<double> cte0 = vars[cte_start + t - 1];
+      AD<double> epsi0 = vars[epsi_start + t - 1];
+      
+      AD<double> delta0 = vars[delta_start + t - 1];
+      AD<double> a0 = vars[a_start + t - 1];  
+
+      // Setup the model constraints
+      fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
+      fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
+      fg[1 + psi_start + t] = psi1 - (psi0 + v0/Lf * delta0 * CppAD::sin(epsi0) * dt);
+      fg[1 + v_start + t] = v1 - (v0 + a0 * dt);
+      fg[1 + cte_start + t] = cte1 - (cte0 + v0 * CppAD::sin(epsi0) * dt); //maybe UPDATE
+      CppAD::AD<double> fp_x0 = 0;
+      // calculate f'(x0)
+      for(int i = 0; i<fp_coeffs.size(); i++)
+        fp_x0 += fp_coeffs[i]*CppAD::pow(x0,i); 
+      
+      fg[1 + epsi_start + t] = epsi1 - (psi0 - CppAD::atan(fp_x0) + v0/Lf * delta0 * dt);
+    }
   }
 };
 
